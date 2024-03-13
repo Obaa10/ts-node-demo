1c7150649a8d5eeec26553b0f02d502a5b28436c
obada <obada@ssg-tech.com>
2024-03-13 15:46:24 +0300

    Create user.ts

---
+++ src/model/user.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose';
+
+export interface IUser {
+    name: string;
+    email: string;
+    password?: string;
+}
+
+const userSchema = new Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+
+export default model<IUser>('User', userSchema);
