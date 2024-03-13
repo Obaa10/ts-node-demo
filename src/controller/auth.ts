2c5a8f01919aae0c4cd752a6a5d027c0fdb7188d
obada <obada@ssg-tech.com>
2024-03-13 15:46:38 +0300

    Create auth.ts

---
+++ src/controller/auth.ts
@@ -0,0 +1,33 @@
+import { hash, compare } from 'bcrypt';
+
+import CustomError from '../utils/error';
+import { getToken } from '../utils/functions';
+import User,{IUser} from '../model/user'
+
+export async function signup(name: string, email: string, password: string) {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) throw new CustomError("Email already registered", 400)
+
+    password = await hash(password, 10);
+    let newUser: IUser  = { name, email, password };
+    newUser = await User.create(newUser);
+
+    delete newUser.password;
+    const token = getToken({ ...newUser, user_type: "client" });
+    return { success: true, message: "Login successful", token, user: newUser };
+
+};
+
+export async function login (email: string, password: string) {
+    let user: IUser = await User.findOne({ email }).select("+password");
+
+    if (!user) throw new CustomError("Invalid credentials", 401);
+
+    const isPasswordValid = await compare(password, user.password ?? "");
+
+    if (!isPasswordValid) throw new CustomError("Invalid credentials", 401);
+
+    delete user.password;
+    const token = getToken({ ...user, user_type: "client" });
+    return { success: true, message: "Login successful", token, user };
+};
