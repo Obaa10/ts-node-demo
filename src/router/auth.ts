d549e6d9f8ccf2fdbe9c7527a75babb1c3b50b67
obada <obada@ssg-tech.com>
2024-03-17 12:17:29 +0300

    Update auth.ts

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,17 +1,17 @@
 import { login, signup } from "./../controller/auth"
 import express, { NextFunction, Request, Response } from 'express';
-import catchAsync from "../utils/catchAsync";
+import catchAsync from "../utils/error/catchAsync";
 
 const router = express.Router();
 
 router.post('/signup', catchAsync(async (req: Request, res: Response) => {
     const { user_name, email, password } = req.body
-    await signup(user_name, email, password)
+    res.json(await signup(user_name, email, password))
 }));
 
 router.post('/login', catchAsync(async (req: Request, res: Response) => {
     const { email, password } = req.body
-    await login(email, password)
+    res.json(await login(email, password))
 }));
 
 
