31fd4c4316c4727680d34d55ef5a82914f17658a
obada <obada@ssg-tech.com>
2024-03-17 12:17:11 +0300

    Update functions.ts

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,7 +1,7 @@
 import { sign } from "jsonwebtoken"
 
 
-export function getToken(user: any) {
+export function generateToken(user: any) {
     return sign(user, "JWT Secure Key", {
         //Optional
         //expiresIn: process.env.USER_JWT_EXPIRE_DURATION,
